cdcae537eb38dbbeefe2a591c15a14f4b5c51fb6
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-06-16 11:22:05 +0530

    add multer

--- src/routes/document-routes.ts
+++ src/routes/document-routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { getDocbyAgreementId, pfiDocuments } from "../controllers/document-controller";
+import upload from "../middleware/upload-middleware";
 
 const documentRouter = Router();
 
 documentRouter.get("/get-docs/:agreementId", getDocbyAgreementId);
-documentRouter.post("/upload-file/:agreementId", pfiDocuments);
+documentRouter.post("/upload-file/:agreementId", upload.array("files", 4) ,pfiDocuments);
 
 export default documentRouter;
